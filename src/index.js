f498f29cde9644f3737df134abbfb414fe333017
gandalf-the-gray <78202277+gandalf-the-gray@users.noreply.github.com>
2023-06-06 14:58:09 +0530

    Add connectDB method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+
+async function connectDB() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.z0byrzc.mongodb.net/`
+    );
+  } catch (e) {
+    //
+  } finally {
+    if (mongoose.connection.readyState !== 1) {
+      setTimeout(connectDB, 1500);
+    } else {
+      console.log('DB connection established');
+    }
+  }
+}
+
+connectDB();
 
 app.listen(PORT, (err) => {
   console.log(err || `listening to port ${PORT}`);
