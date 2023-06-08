498033b69ff65f707bfa8d01c958f858e0518a33
Nitesh Yadav <ny040021@gmail.com>
2023-06-08 15:20:42 +0530

    Add vendor sign-up

---
+++ src/body-validators/vendor/sign-up.js
@@ -0,0 +1,41 @@
+import { BaseValidator, StringField, IntegerField } from 'anubis-inspect';
+import Vendor from '../../models/vendor.js';
+
+const rules = {
+  email: StringField.email('email')
+    .required()
+    .test(async (email) => {
+      const vendor = await Vendor.findOne({ email });
+      return !vendor;
+    }, 'this email is already taken'),
+  name: {
+    first: new StringField('first name').required(),
+    last: new StringField('last name'),
+  },
+  mobile: {
+    country: {
+      code: new IntegerField('country code').required().min(1).max(300),
+      name: new StringField('country name').required().match(/^[a-z\s]*$/i),
+    },
+    number: new IntegerField('mobile number')
+      .required()
+      .test(
+        (number) => String(number).length === 10,
+        'mobile number must have 10 digits',
+      )
+      .test(async (number) => {
+        const vendor = await Vendor.findOne({ 'mobile.number': number });
+        return !vendor;
+      }, 'this mobile number is already taken'),
+  },
+  password: new StringField('password').required().min(8),
+};
+
+class SignUpValidator extends BaseValidator {
+  constructor() {
+    super();
+    super.init(rules);
+  }
+}
+
+export default new SignUpValidator();
--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,14 +1,20 @@
-export function cleanDoc(queryResult) {
+import { deleteObjectKey } from './object.js';
+
+export function cleanDoc(queryResult, paths = []) {
   const doc = queryResult._doc;
   doc.id = doc._id;
   delete doc._id;
   delete doc.__v;
   delete doc.createdAt;
   delete doc.updatedAt;
+  for (let i = 0; i < paths.length; i += 1) {
+    const path = paths[i];
+    deleteObjectKey(doc, path);
+  }
 }
 
-export function getCleanDoc(queryResult) {
-  cleanDoc(queryResult);
+export function getCleanDoc(queryResult, paths = []) {
+  cleanDoc(queryResult, paths);
   return queryResult._doc;
 }
 
@@ -20,6 +26,6 @@ export function addCommonUtils(schema) {
 
   // Clean documents found by queries
   schema.post('init', function preInit() {
-    cleanDoc(this._doc);
+    cleanDoc(this);
   });
 }
