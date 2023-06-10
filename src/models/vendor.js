0380b94c616a25d7e9a1993c809590ab185c9d72
gandalf-the-gray <78202277+gandalf-the-gray@users.noreply.github.com>
2023-06-11 04:57:32 +0530

    Add bcrypt utils

--- src/models/vendor.js
+++ src/models/vendor.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import { getHash } from '../libs/bcrypt.js';
 import { addCommonUtils } from '../utils/mongoose.js';
 
 const schema = new mongoose.Schema(
@@ -24,20 +24,13 @@ const schema = new mongoose.Schema(
 addCommonUtils(schema);
 
 // Hash password before saving
-schema.pre('save', function preSave(next) {
+schema.pre('save', async function preSave(next) {
   const vendor = this;
-  if (!vendor.isModified('password')) {
-    next();
-  } else {
-    bcrypt.hash(vendor.password, 12, (err, hash) => {
-      if (err) {
-        next(err);
-      } else {
-        vendor.password = hash;
-        next();
-      }
-    });
+  if (vendor.isModified('password')) {
+    const hash = await getHash(vendor.password);
+    vendor.password = hash;
   }
+  next();
 });
 
 export default mongoose.model('Vendor', schema);
