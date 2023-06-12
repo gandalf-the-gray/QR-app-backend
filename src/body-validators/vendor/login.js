47553f5ad92ca53627f9b093eb83188da3364051
gandalf-the-gray <78202277+gandalf-the-gray@users.noreply.github.com>
2023-06-13 03:10:38 +0530

    Add vendor login route

--- src/body-validators/vendor/login.js
+++ src/body-validators/vendor/login.js
@@ -1,5 +1,6 @@
 import { BaseValidator, StringField } from 'anubis-inspect';
 import Vendor from '../../models/vendor.js';
+import { verifyHash } from '../../libs/bcrypt.js';
 
 const rules = {
   email: StringField.email('email')
@@ -8,7 +9,17 @@ const rules = {
       const vendor = await Vendor.exists({ email });
       return vendor !== null;
     }, "a user with this email doesn't exist"),
-  password: new StringField('password').required().min(8),
+  password: new StringField('password')
+    .required()
+    .min(8)
+    .dependantTest(
+      async (password, [email]) => {
+        const vendor = await Vendor.findOne({ email });
+        const isPassValid = await verifyHash(password, vendor.password);
+        return [isPassValid, 'incorrect password'];
+      },
+      ['email'],
+    ),
 };
 
 export class LoginValidator extends BaseValidator {
--- src/routes/vendor/auth.js
+++ src/routes/vendor/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { signUp } from '../../controllers/vendor/auth.js';
+import { signUp, login } from '../../controllers/vendor/auth.js';
 
 const router = express.Router();
 
 router.route('/sign-up').post(signUp);
+
+router.route('/login').post(login);
 
 export default router;
